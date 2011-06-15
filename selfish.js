5656bdafd445365f0ea6329ac29f5aedd02d08f9
Irakli Gozalishvili <rfobic@gmail.com>
2011-06-16 01:47:35 +0200

    Base must return __proto__.__proto__ instead of __proto__.

--- selfish.js
+++ selfish.js
@@ -8,7 +8,7 @@
 "use strict";
 
 function Base(prototype) {
-  if (prototype) return Object.getPrototypeOf(prototype);
+  if (prototype) return Object.getPrototypeOf(Object.getPrototypeOf(prototype));
 }
 Base.prototype.new = function() {
   return new this.constructor();
