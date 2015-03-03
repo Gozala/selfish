fbedc7b0a49e379330e0dc4a329d74026753bdab
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2015-03-03 10:00:28 +0100

    Limited browsers in karma to firefox and chrome

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox', 'Opera', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox'],
     // Continuous Integration mode
     singleRun: false
   });
